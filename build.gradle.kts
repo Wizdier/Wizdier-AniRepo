d8f4d6d433b7818bf4a6bc5963238b0e24b6c182
Wizdier <81230118+Wizdier@users.noreply.github.com>
2026-06-09 21:43:32 +0600

    refactor: update Gradle wrapper to version 9.4.1 and clean up buildscript dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,7 @@
-buildscript {
-    dependencies {
-        classpath(libs.kotlin.gradle)
-    }
-}
-
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.serialization) apply false
-
-    alias(kei.plugins.spotless)
+    id("com.android.application") version "9.2.1" apply false
+    id("com.android.library") version "9.2.1" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.21" apply false
 }
 
 val buildLogic: IncludedBuild = gradle.includedBuild("build-logic")
