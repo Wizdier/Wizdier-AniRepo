71a2f73b0719875dd3cc29ce8fae8512b25f0876
Wizdier <81230118+Wizdier@users.noreply.github.com>
2026-06-09 22:18:34 +0600

    refactor: simplify build.gradle.kts by removing unused plugins and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,60 +1,22 @@
-plugins {
-    alias(libs.plugins.kotlin.jvm)
-    alias(libs.plugins.kotlin.samWithReceiver)
-    alias(libs.plugins.spotless)
-    `java-gradle-plugin`
-}
-
-val ktlintVersion = libs.ktlint.bom.get().version
-val editorConfigFile = rootProject.file("../../.editorconfig")
-spotless {
-    kotlin {
-        target("src/**/*.kt", "*.kts")
-        ktlint(ktlintVersion)
-            .setEditorConfigPath(editorConfigFile)
-            .editorConfigOverride(
-                mapOf(
-                    "max_line_length" to 2147483647,
-                ),
-            )
-        trimTrailingWhitespace()
-        endWithNewline()
+buildscript {
+    dependencies {
+        classpath(libs.kotlin.gradle)
     }
 }
 
-dependencies {
-    compileOnly(gradleKotlinDsl())
-    compileOnly(libs.android.gradle)
-    compileOnly(libs.kotlin.gradle)
-    implementation(libs.spotless.gradle)
-    implementation(libs.tapmoc.gradle)
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+
+    alias(kei.plugins.spotless)
 }
 
-samWithReceiver {
-    annotation("org.gradle.api.HasImplicitReceiver")
-}
-
-gradlePlugin {
-    plugins {
-        register("android-base") {
-            id = "kei.plugins.android.base"
-            implementationClass = "PluginAndroidBase"
-        }
-        register("extension") {
-            id = "kei.plugins.extension.legacy"
-            implementationClass = "PluginExtensionLegacy"
-        }
-        register("library") {
-            id = "kei.plugins.library"
-            implementationClass = "PluginLibrary"
-        }
-        register("multisrc") {
-            id = "kei.plugins.multisrc"
-            implementationClass = "PluginMultiSrc"
-        }
-        register("spotless") {
-            id = "kei.plugins.spotless"
-            implementationClass = "PluginSpotless"
+val buildLogic: IncludedBuild = gradle.includedBuild("build-logic")
+tasks {
+    listOf("clean", "spotlessApply", "spotlessCheck").forEach { task ->
+        named(task) {
+            dependsOn(buildLogic.task(":$task"))
         }
     }
 }
